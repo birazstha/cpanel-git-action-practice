9377a17e16b801f39855b27c564ed89d056f827b
biraj1234 <biraj.spell@gmail.com>
2024-02-15 11:49:27 +0545

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        div{
+            text-align: center
+        }
+
+        div > h1{
+            font-size: 3rem;
+        }
+    </style>
     <title>Document</title>
 </head>
+
 <body>
-    <h1>Hello World.</h1>
+    <div>
+        <h1>Hello World.</h1>
+    </div>
 </body>
+
 </html>
