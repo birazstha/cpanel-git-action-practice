9cb2b34a21ef40d3297e2f683dd9480c328f4d7c
biraj1234 <biraj.spell@gmail.com>
2024-02-15 12:20:47 +0545

    changes in yml

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,12 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <style>
-        div{
+        div {
             text-align: center
         }
 
-        div > h1{
+        div>h1 {
             font-size: 3rem;
+            color: red;
         }
     </style>
     <title>Document</title>
